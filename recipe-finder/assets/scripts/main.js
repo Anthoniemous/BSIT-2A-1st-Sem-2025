e89bfa4224f223c307de45ef45caccd986119a0f
My Name <myemail@example.com>
2025-04-08 13:53:04 +0800

    mao ni api

--- recipe-finder/assets/scripts/main.js
+++ recipe-finder/assets/scripts/main.js
@@ -8,7 +8,18 @@ const drinksButton = document.querySelector("#drinks-search-button");
 const missionSection = document.querySelector("#mission-section");
 
 // Array of ingredients to choose from randomly
-let possibleFoodIngredients = ["cheese", "beef", "steak", "chicken", "bread", "broccoli", "jalapeño", "mango", "apple", "banana"];
+let possibleFoodIngredients = [
+  "cheese",
+  "beef",
+  "steak",
+  "chicken",
+  "bread",
+  "broccoli",
+  "jalapeño",
+  "mango",
+  "apple",
+  "banana",
+];
 
 // Variables for food API
 const foodURL = "https://api.edamam.com/api/recipes/v2?type=public&q=";
@@ -16,35 +27,40 @@ const appIDKey = "&app_id=99f65177&app_key=ecb411eb41e5416150875af0c19ffec7";
 
 // Choose one of the ingredients randomly
 function getRandomIngredient() {
-    let ingredient = possibleFoodIngredients[Math.floor(Math.random() * possibleFoodIngredients.length)];
-    getFoodRecipe(ingredient);
+  let ingredient =
+    possibleFoodIngredients[
+      Math.floor(Math.random() * possibleFoodIngredients.length)
+    ];
+  getFoodRecipe(ingredient);
 }
 
 // Call on page load to have new featured recipes each time
 getRandomIngredient();
 
-
 // Request data from food API using the random ingredient
 function getFoodRecipe(ingredient) {
-    fetch(foodURL + ingredient + appIDKey + "&random=true")
-    .then(function(response) {
-        if (response.ok) {
-            return response.json();
-        }
+  fetch(foodURL + ingredient + appIDKey + "&random=true")
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
     })
-    .then(function(data) {
-        if (data.count !== 0) {
-            // Get returned list of recipes from random ingredient, then choose a random one of those
-            let allReturnedRecipes = data.hits;
-            let randomFromReturned = allReturnedRecipes[Math.floor(Math.random() * allReturnedRecipes.length)];
-            let randomRecipeInfo = {
-                name: randomFromReturned.recipe.label,
-                image: randomFromReturned.recipe.image,
-                url: randomFromReturned.recipe.url
-            }
-            console.log(randomRecipeInfo);
-        }
-    })
+    .then(function (data) {
+      if (data.count !== 0) {
+        // Get returned list of recipes from random ingredient, then choose a random one of those
+        let allReturnedRecipes = data.hits;
+        let randomFromReturned =
+          allReturnedRecipes[
+            Math.floor(Math.random() * allReturnedRecipes.length)
+          ];
+        let randomRecipeInfo = {
+          name: randomFromReturned.recipe.label,
+          image: randomFromReturned.recipe.image,
+          url: randomFromReturned.recipe.url,
+        };
+        console.log(randomRecipeInfo);
+      }
+    });
 }
 
 // TODO:
