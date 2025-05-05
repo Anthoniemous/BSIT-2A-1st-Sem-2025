766a9564c60ca311151e55d086e88200386fc238
peterGwapo29 <peterolanolan@gmail.com>
2025-05-05 21:09:29 +0800

    print/echo assignment

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    
+
+    <?php
+        $firstName = "Francine Grace";
+        $lastName = "Poraso";
+        $message = "Welcome to PHP programming!";
+
+        echo "First Name: $firstName<br>";
+        echo "Last Name: $lastName<br>";
+
+        print "Message: $message";
+    ?>
+
+
+</body>
+</html>
