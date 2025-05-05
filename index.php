2214828af17c1843c984afff0fc05e35e00bea01
peterGwapo29 <peterolanolan@gmail.com>
2025-05-05 21:00:06 +0800

    echo and print assignment

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $fname = "Mekylla";
+    $lname = "Taghoy";
+    $Mname = "Banez";
+    $msg = "Watsap mananap!";
+
+    echo "<strong>Fullname: $fname $Mname $lname </strong>";
+    print "<br><strong>Message:</strong> $msg";
+
+?>
