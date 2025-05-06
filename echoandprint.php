1c67fb707b6d3cbd89e11ac761bd75e252f61892
juhane03 <juhane03>
2025-05-06 14:29:38 +0800

    php assigment

---
+++ echoandprint.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            echo "Hello Echo!<br>";
+            echo "I am", "Angel", "Wenceslao.<br><br>";
+        ?>
+
+        <?php
+            print "Hello Print!<br>";
+            print "I am Angel Wenceslao.<br>";
+        ?>
+    </body>
+</html>
