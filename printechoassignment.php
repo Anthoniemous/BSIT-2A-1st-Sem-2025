fd7ac54df95bb6386811d1f27001b8c73f87941d
BarroJohnNems <barrojohnnems01@gmail.com>
2025-05-07 13:46:19 +0800

    php assignment

---
+++ printechoassignment.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    </head>
+        <title>Echo and Print</title>
+    </head>
+    <body>
+        <?php
+            echo "<strong>Hello Echo!<strong>";
+            echo "ako si angkol ", "John ", "Barro.<br>";
+        ?>
+        <?php
+            print "Hello sa Print!";
+            print "I am John <strong>Barro.<strong><br>";
+        ?>
+        
+    </body>
+</html>
