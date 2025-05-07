dab864316a292914bfd5c48853e8c1e0a9e99347
juhane03 <juhane03>
2025-05-07 13:28:03 +0800

    php assignment

---
+++ echoandprint.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            echo "Hello Echo!<br>";
+            echo "I am", "Ruela", "Joy", "Cruz.<br>";
+        ?>
+
+        <?php
+            print "Hello Print!<br>";
+            print "I am Ruela Joy Cruz.<br>";
+        ?>
+    </body>
+</html>
