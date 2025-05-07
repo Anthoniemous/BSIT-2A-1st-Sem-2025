01ddd2f5dc642b0c17aedc5590b8a6f8bca37602
Adrian Macalolot <adrianpaul201211@gmail.com>
2025-05-07 23:23:14 +0800

    My PHP project initial commit

---
+++ macalolot.php
@@ -0,0 +1,9 @@
+<?php
+      $hello = "Hello";
+      $name = "Adrian";
+                
+         echo "Using Echo<br>";
+         echo "$hello!, I am $name and this is my php project<br><br><br>";      
+         print "Using Print<br>";
+         print "$hello!, I am $name and this is my php project";
+ ?>
