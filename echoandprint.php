e70026231ce27e211625469fc8341faf62d6ab8b
juhane03 <juhanemamucas087@gmail.com>
2025-05-07 13:52:05 +0800

    php assignment

---
+++ echoandprint.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Hello Echo! <br>";
+            echo "I am", "Wella", "Lyn", "Sanico<br>";
+        ?>
+        
+        <?php
+            print "Hello Print!<br>";
+            print "I am Wella lyn B. Sanico <br>";
+        ?>
+    <body>
+<html>
