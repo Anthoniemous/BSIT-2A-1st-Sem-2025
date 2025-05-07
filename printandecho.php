547966f483807e28c98f7caa830ddaedf23c1264
juhane03 <juhane03>
2025-05-07 14:35:11 +0800

    assignment php-altivo

---
+++ printandecho.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            echo "I am", "Fate", "Altivo", "and this Echo!";
+        ?>
+
+        <?php
+            print "Hello Print!<br>";
+            print "I am Fate Altivo.<br>";
+        ?>
+    </body>
+</html>
