f1729ba8f109b551e01ed1a64b93d74d099cb04a
juhane03 <juhanemamucas087@gmail.com>
2025-05-07 14:02:08 +0800

    php assignment

---
+++ echoandprint.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Hello Echo! <br>";
+            echo "I am", "Juhane", "MAmucas<br>";
+        ?>
+        
+        <?php
+            print "Hello Print!<br>";
+            print "I am Juhane Mamucas <br>";
+        ?>
+    <body>
+<html>
