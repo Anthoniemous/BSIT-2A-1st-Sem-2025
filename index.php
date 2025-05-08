8987521b9e41fbc578d47217b2f7130ccbfaf250
Wardiee <verjhn@gmail.com>
2025-05-08 14:08:44 +0800

    echo/print

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Archie";
+$age = 20;
+$hobby = "coding";
+
+echo "<h1>Introducing Myself</h1>";
+echo "Hello, my name is $name.<br>";
+echo "I am $age years old.<br>";
+echo "One of my hobbies is $hobby.<br>";
+
+
+print "<h2>More About Me</h2>";
+print "I enjoy learning new things.<br>";
+print "PHP is !fun to learn!<br>";
+print "Thank you for visiting my introduction page.<br>";
+?>
